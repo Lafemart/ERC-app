f37f8b1da47b7a755c8c7517e6e2f3a8bfceadb3
mayowa86 <mayowasamuel86@gmail.com>
2022-03-10 17:44:47 +0100

    edited auth

--- src/Context/auth-context.js
+++ src/Context/auth-context.js
@@ -43,18 +43,6 @@ export const AuthContextProvider = (props) => {
 
 export default AuthContext;
 
-
-
-/* We export default context to enable the usage in the components in the app
--any component that want to make of this context must be wrapped by it
--Likewise any component that wants to mak use of the context must listen to it
-using the compose atribute as well
--be still if we need the context in all components in the application we just wrap
-our app component with it at once. and we use it everywhere
--know that we can just with context alone because its not a component, so it cant wrap
-other component, instead we use 
-export default AuthContext;
-
 /* We export default context to enable the usage in the components in the app
 -any component that want to make of this context must be wrapped by it
 -Likewise any component that wants to mak use of the context must listen to it
